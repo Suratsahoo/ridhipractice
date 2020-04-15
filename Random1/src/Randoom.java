225f18ce6bdc5185fca49db9474c24f882302c73
Ridhi <surat@192.168.0.28>
2020-04-15 23:36:36 +0530

    My new Trial

---
+++ Random1/src/Randoom.java
@@ -0,0 +1,16 @@
+
+public class Randoom {
+
+	public Randoom() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hello");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		Randoom ob=new Randoom();
+
+	}
+
+}
